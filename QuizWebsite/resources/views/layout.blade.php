f14172782119398a7a255915380360e41cb884ed
dhruba <adhroba@gmail.com>
2025-07-09 23:13:23 +0600

    extend

--- QuizWebsite/resources/views/layout.blade.php
+++ QuizWebsite/resources/views/layout.blade.php
@@ -17,5 +17,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Flatpickr JS -->
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    @stack('scripts')
 </body>
 </html>
