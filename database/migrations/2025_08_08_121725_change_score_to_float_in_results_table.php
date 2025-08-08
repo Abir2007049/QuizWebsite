61fe84f94a08c750d471fe9de920c85aaac75c2f
Abir049 <abirjghs1877@gmail.com>
2025-08-08 15:41:27 +0600

    beta 3 with modifications

--- database/migrations/2025_08_08_121725_change_score_to_float_in_results_table.php
+++ database/migrations/2025_08_08_121725_change_score_to_float_in_results_table.php
@@ -4,25 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('results', function (Blueprint $table) {
-            //
+            $table->float('score', 5, 2)->change(); // 5 digits total, 2 after decimal
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('results', function (Blueprint $table) {
-            //
+            $table->integer('score')->change(); // rollback to integer if needed
         });
     }
 };
